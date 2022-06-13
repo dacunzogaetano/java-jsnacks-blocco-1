2add39498ed3a3128925575bd1f856bf00730365
dacunzogaetano <dacunzogaetano@gmail.com>
2022-06-13 21:20:23 +0200

    quarto JSnack

--- src/jsnacks/JSnack3.java
+++ src/jsnacks/JSnack3.java
@@ -9,18 +9,23 @@ public class JSnack3 {
 		// Generatore di "nomi cognomi" casuali: prendendo una lista di nomi
 		// e una lista di cognomi, genera una falsa lista di 3 invitati
 		
-		String[] nomi = {"Luciano", "Vasco", "Tiziano", "Max", "Claudio"};
-		String[] cognomi = {"Ligabue","Rossi","Ferro", "Pezzali", "Baglioni"};			
+		String[] nomi = {"Luciano", "Vasco", "Tiziano", "Max", "Claudio", "Lucio"};
+		String[] cognomi = {"Ligabue","Rossi","Ferro", "Pezzali", "Baglioni", "Dalla"};			
+		String[] Lista = new String [3];
 		
 		Random rand = new Random();
 		
+		{System.out.println("Gli invitati sono: ");}
+		
 		  
 	    for	(int i=0; i<3; i++) {
-	    	String nome = nomi[rand.nextInt(5)];
-	    	String cognome = cognomi[rand.nextInt(5)];
-	    				    	
-	    	System.out.println("Gli invitati sono: " + nome + " " + cognome);
-	    }
+	    	int nom = rand.nextInt(nomi.length);
+	    	int cogn = rand.nextInt(cognomi.length);
+	    	
+	    			
+	    	
+	    	System.out.println(nomi[nom] + " " + cognomi[cogn]);
+	    }	
 		
 			
 
